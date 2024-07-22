0d215ff2f7772c239b06c4e883774c8f7811cfad
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-07-22 14:28:41 +0530

    Time: 11 ms (94.85%) | Memory: 44.9 MB (80.37%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,37 +1,47 @@
-import java.util.*;
-
 class Solution {
-    public int calculate(String s) 
-    {
-        ArrayDeque<Integer> stk = new ArrayDeque<>();
-        char sign = '+';
-        int v = 0;
-        
-        for (int i = 0; i < s.length(); i++) 
-        {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                v = v * 10 + (c - '0');
-            }
-            if (i == s.length() - 1 || c == '+' || c == '-' || c == '*' || c == '/') {
-                if (sign == '+') {
-                    stk.push(v);
-                } else if (sign == '-') {
-                    stk.push(-v);
-                } else if (sign == '*') {
-                    stk.push(stk.pop() * v);
-                } else {
-                    stk.push(stk.pop() / v);
-                }
-                sign = c;
-                v = 0;
-            }
-        }
-        int ans = 0;
-        while (!stk.isEmpty()) {
-            ans += stk.pop();
-        }
-        return ans;
+    int result = 0;
+    int previousNumber = 0;
+    int currentNumber = 0;
 
+    public int calculate(String s) {
+        char operator = '+';
+        int currentNumber = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char nextChar = s.charAt(i);
+
+            if(Character.isDigit(nextChar)){
+                currentNumber*=10;
+                currentNumber += (int) nextChar - '0';
+            } else if (!Character.isWhitespace(nextChar)) {
+                pushToStack(currentNumber, operator, stack);
+                currentNumber = 0;
+                operator = nextChar;
+            }
+        }
+
+        pushToStack(currentNumber, operator, stack);
+
+        return result + previousNumber;
+    }
+
+    private void pushToStack(int currentNumber, char operator, Stack<Integer> stack){
+        switch(operator){
+            case '+': 
+                result += previousNumber;
+                previousNumber = currentNumber;
+                break;
+            case '-':
+                result += previousNumber;
+                previousNumber = currentNumber*-1;
+                break;
+            case '/':
+                previousNumber /= currentNumber;
+                break;
+            case '*':
+                previousNumber *= currentNumber;
+                break;
+        }
     }
 }
