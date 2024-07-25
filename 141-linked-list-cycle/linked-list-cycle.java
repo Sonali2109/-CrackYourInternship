029a8acc51830d8340f794a5675424ca3e559ea5
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-07-25 23:27:06 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (91.76%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean containsCycle = false;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) {
+                containsCycle = true;
+                break;
+            }
+        }
+        
+        // If no cycle found
+        if (!containsCycle) {
+            return false;
+        }
+
+        return true;
+    }
+}
