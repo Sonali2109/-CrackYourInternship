471be614e7bee92fd82933c35ee49599b9e8ba8f
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-07-27 21:11:33 +0530

    Time: 2 ms (67.09%) | Memory: 44.8 MB (10.28%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
+        ArrayDeque<Integer> stack1 = new ArrayDeque<>();
+        ArrayDeque<Integer> stack2 = new ArrayDeque<>();
         while (l1 != null) {
             stack1.push(l1.val);
             l1 = l1.next;
@@ -23,7 +23,7 @@ class Solution {
 
         int carry = 0;
         ListNode dummyHead = null;
-        
+
         while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
             int val1 = stack1.isEmpty() ? 0 : stack1.pop();
             int val2 = stack2.isEmpty() ? 0 : stack2.pop();
