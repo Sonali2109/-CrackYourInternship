0e85fab588a8002e5f9888fbbe09ff5c452eacf1
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-07-29 22:05:04 +0530

    Time: 7 ms (88.59%) | Memory: 79.2 MB (82.37%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.java
+++ 1622-max-value-of-equation/max-value-of-equation.java
@@ -1,18 +1,32 @@
 class Solution {
-    public int findMaxValueOfEquation(int[][] p, int k) 
+    public int findMaxValueOfEquation(int[][] points, int k) 
     {
-        Deque<Integer> d = new ArrayDeque<>();
-        int res = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE; //assign the lowest number
+        int res = 0;
+        int mod = 0;
+        int flag = 1;
 
-        for (int j = 0; j < p.length; ++j) {
-            while (!d.isEmpty() && p[j][0] - p[d.peek()][0] > k)
-                d.poll();
-            if (!d.isEmpty())
-                res = Math.max(res, p[d.peek()][1] - p[d.peek()][0] + p[j][1] + p[j][0]);
-            while (!d.isEmpty() && p[d.peekLast()][1] - p[d.peekLast()][0] < p[j][1] - p[j][0])
-                d.pollLast();
-            d.addLast(j);
-        }    
-        return res;
+        for(int i = 0;i<points.length-1;i++) //iteration over the points
+        {
+            if(flag<i+1)
+                flag = i+1;
+                
+            for(int j = flag;j<points.length;j++) 
+			//iteration over the points again so that we can deliver the variables in the equation
+            {
+                mod = points[i][0]-points[j][0]; 
+                if(mod<0)
+                    mod = -mod;
+                if(mod>k)
+                    break;
+                res = points[i][1]+points[j][1]+mod; //final equation 
+                if(max<res)
+                {
+                    max = res; //updation of max
+                    flag = j-1;
+                }
+            }
+        }
+        return max;
     }
 }
