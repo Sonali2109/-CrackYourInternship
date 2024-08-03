dc1f8c75f998f18659e8062c39c0120e37c839cd
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-08-03 15:42:57 +0530

    Time: 119 ms (5.49%) | Memory: 45.5 MB (42.43%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,16 +1,21 @@
 class Solution {
-    public int minPathSum(int[][] grid) 
-    {
-        int m = grid.length, n = grid[0].length;
-        int dp[][] = new int[m][n];
-        return find(grid, m - 1, n - 1, dp);
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m];
+        return func(grid,n-1,m-1,dp);
     }
+    public int func(int[][] arr,int i,int j,int[][] dp){
+        if(i < 0 || j < 0)return (int)Math.pow(10,9);
+        if(i == 0 && j == 0){
+            return arr[i][j];
+        }
 
-    public static int find(int[][] grid, int m, int n, int dp[][]) 
-    {
-        if (m == 0 && n == 0)    return grid[m][n];
-        else if (m < 0 || n < 0)    return Integer.MAX_VALUE;
-        else if (dp[m][n] != 0)    return dp[m][n];
-        return dp[m][n] = grid[m][n] + Math.min(find(grid, m - 1, n, dp), find(grid, m, n - 1, dp));
+        if(dp[i][j] != 0)return dp[i][j];
+
+        int l = arr[i][j] + func(arr,i,j-1,dp);
+        int u = arr[i][j] + func(arr,i-1,j,dp);
+
+        return dp[i][j] = Math.min(l,u);
     }
 }
