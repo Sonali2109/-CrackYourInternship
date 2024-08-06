6a983b17300f0b8f6b79d842063fc636852cc91b
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-08-06 22:33:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (75.80%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -1,46 +1,35 @@
 class Solution {
-    private boolean isValid(int[][] grid, int row, int col) {
-        if (row >= grid.length || row < 0 || col >= grid[0].length || col < 0) return false;
-        if (grid[row][col] == -1) return false;
-        return true;
-    }
+    int nonObs=1,res;
+    int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
+    void backtracking(int[][] grid, int i,int j,int count){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==-1) return;
 
-    private int getUniquePaths(int[][] grid, int[][] diff, int row, int col, int step) {
-        if (grid[row][col] == 2) {
-            if (step == -1) return 1;
-            return 0;
+        if(grid[i][j]==2){
+            if(count==nonObs) res++;
+            return;
         }
-
-        int count = 0;
-        grid[row][col] = -1; // assigning to the current standing as if I mark next then I will never reach the goal(2) as it will be turned into (-1).
-        for (int[] choice : diff) {
-            int newRow = row + choice[0];
-            int newCol = col + choice[1];
-            if (isValid(grid, newRow, newCol)) {
-                count += getUniquePaths(grid, diff, newRow, newCol, step - 1);
-            }
+        grid[i][j]=-1;
+        for(int[] dir:dirs){
+            int i_=i+dir[0]; int j_=j+dir[1];
+            backtracking(grid,i_,j_,count+1);
         }
-        grid[row][col] = 0;
-
-        return count;
+        grid[i][j]=0;
     }
-
+    
     public int uniquePathsIII(int[][] grid) {
-        int[][] diff = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
-
-        int sr = 0;
-        int sc = 0;
-        int steps = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    sr = i;
-                    sc = j;
-                } else if (grid[i][j] == 0) steps++;
+        int m=grid.length;
+        int n=grid[0].length;
+        int x=0, y=0; 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0) nonObs++;
+                else if(grid[i][j]==1){
+                    x=i;y=j;
+                }
             }
-        }
-        
-        return getUniquePaths(grid, diff, sr, sc, steps);
+        }    
+        int count=0;  
+         backtracking(grid,x,y,count);
+         return res;
     }
 }
