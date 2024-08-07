a2a9793c520aa35e36ee2a1b4862f171faf095e6
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-08-07 12:46:13 +0530

    Time: 35 ms (67.43%) | Memory: 40.4 MB (49.83%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -1,25 +1,30 @@
 class Solution {
-
-    private int count = 0;
-
-    private void calculate(int n, int pos, boolean[] visited)
-    {
-        if(pos>n)  count++;
-
-        for(int i=1; i<=n; i++)
-        {
-            if(!visited[i] && (pos % i == 0 || i % pos == 0))
-            {
-                visited[i] = true;
-                calculate(n, pos+1, visited);
-                visited[i] = false; //backtrack if no suitable condition is true !
-            } 
-        }
-    }
+    public static int count;
 
     public int countArrangement(int n) {
-        boolean[] visited = new boolean[n+1];
-        calculate(n,1,visited);
+        count = 0;
+        int[] perm = new int[n+1];
+        backtrack(perm, 1);
+
         return count;
+    }
+
+    public void backtrack(int[] perm, int pos) {
+        if(pos == perm.length)
+        {
+            count++;
+
+            return;
+        }
+
+        for(int i = 1; i < perm.length; i++)
+        {
+            if(perm[i] == 0 && (i%pos == 0 || pos%i == 0))
+            {
+                perm[i] = pos;
+                backtrack(perm, pos+1);
+                perm[i] = 0;
+            }
+        }
     }
 }
