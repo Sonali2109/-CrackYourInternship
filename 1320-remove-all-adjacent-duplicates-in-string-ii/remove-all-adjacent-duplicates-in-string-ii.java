ef761b2b925060d0c4ba7317bcb8edf41a50ac5b
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-08-13 21:56:11 +0530

    Time: 10 ms (95.45%) | Memory: 47.1 MB (12.21%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,23 +1,28 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-     Stack<Character> charSt=new Stack<>();
-        Stack<Integer> countSt=new Stack<>();
-        
-        for(char ch:s.toCharArray()){
-            if(charSt.size()>0 && charSt.peek()==ch) countSt.push(countSt.peek()+1);
-            else countSt.push(1);
-            
-            charSt.push(ch);
-            if(countSt.peek()==k){
-                for(int i=0;i<k;i++){
-                    charSt.pop();
-                    countSt.pop();
-                }
+        // ArrayDeque has better performance than Stack and LinkedList
+        ArrayDeque<Adjacent> st = new ArrayDeque<>(s.length());
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && st.peekLast().ch == c) {
+                st.peekLast().freq++; // Increase the frequency
+            } else {
+                st.addLast(new Adjacent(c, 1));
             }
+            if (st.peekLast().freq == k) // If reach enough k duplicate letters -> then remove
+                st.removeLast();
         }
-        
-        StringBuilder sb=new StringBuilder();
-        while(charSt.size()>0) sb.append(charSt.pop());
-        return sb.reverse().toString();
+        StringBuilder sb = new StringBuilder();
+        for (Adjacent a : st) {
+            sb.append(String.valueOf(a.ch).repeat(a.freq));
+        }
+        return sb.toString();
+    }
+    class Adjacent {
+        char ch;
+        int freq;
+        public Adjacent(char ch, int freq) {
+            this.ch = ch;
+            this.freq = freq;
+        }
     }
 }
