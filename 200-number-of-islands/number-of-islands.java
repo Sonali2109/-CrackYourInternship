5367aac5cee38b9c6c069967b9ba7cac4df2a814
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-08-20 23:34:57 +0530

    Time: 2 ms (99.64%) | Memory: 49.1 MB (85.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,40 +1,29 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int islands = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Set<String> visited = new HashSet<>();
-
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1' && !visited.contains(r + "," + c)) {
-                    islands++;
-                    bfs(grid, r, c, visited, directions, rows, cols);
-                }
-            }
+    public void solve(char[][]grid,int n,int m,int i,int j){
+        if(i==-1||j==-1||i==n||j==m ||grid[i][j]=='0'){
+            return;
         }
-
-        return islands;        
+        else{
+            grid[i][j]='0';
+            solve(grid,n,m,i,j-1);
+            solve(grid,n,m,i+1,j);
+            solve(grid,n,m,i-1,j);
+            solve(grid,n,m,i,j+1);
+        }
     }
-
-    private void bfs(char[][] grid, int r, int c, Set<String> visited, int[][] directions, int rows, int cols) {
-        Queue<int[]> q = new LinkedList<>();
-        visited.add(r + "," + c);
-        q.add(new int[]{r, c});
-
-        while (!q.isEmpty()) {
-            int[] point = q.poll();
-            int row = point[0], col = point[1];
-
-            for (int[] direction : directions) {
-                int nr = row + direction[0], nc = col + direction[1];
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && !visited.contains(nr + "," + nc)) {
-                    q.add(new int[]{nr, nc});
-                    visited.add(nr + "," + nc);
+    public int numIslands(char[][] grid) {
+        int n,m;
+        n=grid.length;
+        m=grid[0].length;
+        int count=0;
+        int i,j;
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    solve(grid,n,m,i,j);
                 }
             }
-        }
-    }    
+        }return count;
+    }
 }
