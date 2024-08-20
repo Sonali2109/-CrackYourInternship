7ce833ee16ba421dead8557f037f06c8293e2f14
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-08-20 23:17:32 +0530

    Time: 166 ms (89.28%) | Memory: 89.3 MB (83.02%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -1,40 +1,61 @@
 class WordDictionary {
-    Map<Integer, List<String>> map = new HashMap<>();
-public void addWord(String word) {
-    int index = word.length();
-	if (!map.containsKey(index)) {
-	    List<String> list = new ArrayList<>();
-	    list.add(word);
-		map.put(index, list);
-	} else {
-		map.get(index).add(word);
-	}
-}
-
-
-public boolean search(String word) {
-  int index = word.length();
-  if (!map.containsKey(index)) {
-      return false;
-  }
-  
-  List<String> list = map.get(index);
-  for(String s : list) { 
-      if(isSame(s, word)) { // when word has '.'
-          return true;
-      }
-  }
-  return false;
-}
-
-public boolean isSame(String s, String word) { // word has '.'
-    for (int i = 0; i < s.length(); i++) {
-        if (word.charAt(i) != '.' && s.charAt(i) != word.charAt(i)) {
-            return false;
+    
+    class TrieNode {
+        private TrieNode children[];
+        private boolean isWord;
+        
+        public TrieNode(){
+            children = new TrieNode[26];
         }
     }
-    return true;
-}
+    
+    private TrieNode root;
+    public WordDictionary() {
+        
+        root = new TrieNode();
+        
+    }
+    
+    public void addWord(String word) {
+        
+        TrieNode curr = root;
+        
+        for(char ch : word.toCharArray()) {
+            
+            if(curr.children[ch-'a'] == null) {
+                TrieNode node = new TrieNode();
+                curr.children[ch-'a'] = node;
+            }
+            curr = curr.children[ch-'a'];
+        }
+        curr.isWord = true;
+    }
+    
+    public boolean search(String word) {
+        //since we have "." chars in this one, we will use dfs
+        return match(root, 0, word.toCharArray());
+        
+    }
+    
+    
+    public boolean match(TrieNode node, int i, char[] word) {
+        
+        //we reached end of word char array, we return if it is a word
+        if(i == word.length) return node.isWord;
+        
+        //encounter a ., then dfs char in the children array and see if there is a char availanle
+        if(word[i] == '.') {
+                for(int j=0; j<node.children.length; j++) {
+                    if(node.children[j] != null && match(node.children[j], i+1, word)) {
+                        return true;
+                    }
+            }        
+        }
+        else {
+                return node.children[word[i] - 'a'] !=null && match(node.children[word[i] - 'a'], i+1, word);
+            }
+            return false;
+    }
 }
 
 /**
