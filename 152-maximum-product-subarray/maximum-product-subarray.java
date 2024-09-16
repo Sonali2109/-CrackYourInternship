ea574dfd7386d1b855199bca648cee2d0d43e114
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-09-16 19:02:53 +0530

    Time: 2 ms (69.43%) | Memory: 44.8 MB (44.23%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,6 +1,44 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        
+        int n = nums.length;
+        double left = 1;
+        double right = 1;
+        double ans = nums[0];   //2 --> 2 --> 6
+
+        for(int i=0; i<n; i++)
+        {
+            if(left == 0)   left = 1;
+            if(right == 0)  right = 1;
+
+            left = left * nums[i]; //2 --> 6
+            right = right * nums[n-1-i];      //-2 --> -4
+
+            ans = Math.max(ans, Math.max(left,right));
+        }
+
+        return (int) ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
         int n = nums.length;
         double left = 1;
         double right = 1;
@@ -18,5 +56,4 @@ class Solution {
         }
 
         return (int) ans;
-    }
-}
+*/
