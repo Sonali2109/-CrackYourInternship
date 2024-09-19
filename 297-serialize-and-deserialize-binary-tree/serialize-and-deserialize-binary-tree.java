af2335b14cbc5a68d3ed35352e57168a9645beb5
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-09-20 02:32:37 +0530

    Time: 5 ms (97.37%) | Memory: 45.4 MB (84.47%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -7,66 +7,122 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Codec {
 
+public class Codec 
+{
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        String res = "";
-        if (root == null) {
-            return res;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            if (node == null) {
-                res = res + "n ";
-            } else {
-                res = res + node.val + " ";
-            }
-            if (node != null) {
-                queue.offer(node.left);
-                queue.offer(node.right);
-            }
-        }
-        System.out.print(res);
-        return res;
+        StringBuilder sb = new StringBuilder(); 
+        preorder(root, sb);      
+        return sb.toString();
     }
 
     // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) 
-    {
-        if (data.isEmpty()) {
-            return null;
+    public TreeNode deserialize(String data) {
+        TreeNode root = null;
+
+        if(data == null || data.isEmpty())
+        {
+            return root; 
         }
 
-        String[] nodeValues = data.split(" ");
-        TreeNode node = new TreeNode(Integer.parseInt(nodeValues[0]));
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(node);
-        
-        for(int i = 1; i < nodeValues.length; i++) {
-            TreeNode parentNode = queue.poll();
-            if (!nodeValues[i].equals("n")) {
-                parentNode.left = new TreeNode(Integer.parseInt(nodeValues[i]));
-                queue.offer(parentNode.left);
-            }
-            if (!nodeValues[++i].equals("n")) {
-                parentNode.right = new TreeNode(Integer.parseInt(nodeValues[i]));
-                queue.offer(parentNode.right);
-            }
-        }
-        return node;
+        return des(0,data).getKey();
     }
 
-    public static void inorder(TreeNode root) {
-        if (root == null) {
-            return;
+    private Pair<TreeNode,Integer> des(int index, String data)
+    {
+        TreeNode node = new TreeNode();
+        node.val = Integer.parseInt(data.substring(index, index+5));
+
+        index = index+5; 
+
+        if(index >= data.length())
+        {
+            return new Pair<>(node,index);
         }
-        inorder(root.left);
-        System.out.print(root.val + " ");
-        inorder(root.right);
+
+        if(data.charAt(index) == '(')
+        {
+            index++; 
+            if(data.charAt(index) == 'L')
+            {
+                Pair<TreeNode,Integer> res = des(index+1, data);
+                node.left = res.getKey();
+                index = res.getValue();
+            }
+
+            if(data.charAt(index) == 'R')
+            {
+                Pair<TreeNode,Integer> res = des(index+1, data);
+                node.right = res.getKey();
+                index = res.getValue();
+            }
+
+            // skip )
+            return new Pair<>(node,index+1);
+        }
+
+        return new Pair<>(node, index);
+    }
+
+    private void preorder(TreeNode node, StringBuilder sb)
+    {
+        if(node == null)
+        {
+            return; 
+        }
+
+        if(node.val > 0)
+        {
+            sb.append("+");
+        }
+        else 
+        {
+            sb.append("-");
+        }
+
+        node.val = Math.abs(node.val);
+
+        if(node.val < 1000 && node.val > 99)
+        {
+            sb.append("0");
+        }
+
+        else if(node.val <= 99 && node.val > 9)
+        {
+            sb.append("00");
+        }
+
+        else if(node.val <= 9)
+        {
+            sb.append("000");
+        }
+
+        sb.append(node.val);
+
+        if(node.left != null || node.right != null)
+        {
+            sb.append("(");
+        }
+
+        if(node.left!=null)
+        {
+            sb.append("L");
+            preorder(node.left, sb);
+        }
+
+        if(node.right != null)
+        {
+            sb.append("R");
+            preorder(node.right, sb);
+        }
+
+        if(node.left != null || node.right != null)
+        {
+            sb.append(")");
+        }
+
+        return;
     }
 }
 
