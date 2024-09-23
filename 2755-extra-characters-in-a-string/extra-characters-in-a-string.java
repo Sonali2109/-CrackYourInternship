460c2e089a180a03e53a5547295ed2abe91199b5
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-09-23 21:14:05 +0530

    Time: 7 ms (95.92%) | Memory: 45.2 MB (54.42%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,28 +1,62 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set; 
+class TrieNode {
+    TrieNode[] children;
+    boolean isWord;
+
+    TrieNode() {
+        children = new TrieNode[26];
+        isWord = false;
+    }
+}
 
 class Solution {
-    public int minExtraChar(String s, String[] dictionary) 
-    {
-        int n = s.length();
-        Set<String> dict = new HashSet<>();
-        for (String word : dictionary) {
-            dict.add(word);
-        }
-
-        int[] dp = new int[n + 1];
-
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = dp[i + 1] + 1; // Worst case: add one character
-            for (int j = i; j < n; j++) {
-                String substring = s.substring(i, j + 1);
-                if (dict.contains(substring)) {
-                    dp[i] = Math.min(dp[i], dp[j + 1]);
-                }
-            }
-        }
-
+    int[] dp;
+    TrieNode root;
+    public int minExtraChar(String s, String[] dictionary) {
+        int len = s.length();
+        root = buildTrieNode(dictionary);
+        dp = new int[len+1];
+        Arrays.fill(dp, -1);
+        helperFunction(s, 0, len);
         return dp[0];
     }
+
+    public int helperFunction(String s, int currIdx, int len) {
+        if(currIdx == len) {
+            return 0;
+        }
+        if(dp[currIdx] != -1) {
+            return dp[currIdx];
+        }
+
+        TrieNode node = root;
+        int ans = 1 + helperFunction(s, currIdx+1, len);
+        for(int i = currIdx; i < len; i++) {
+            int ch = s.charAt(i)-'a';
+            if(node.children[ch] == null) {
+                break;
+            }
+            node = node.children[ch];
+            if(node.isWord == true) {
+                ans = Math.min(ans, helperFunction(s, i+1, len));
+            }
+        }
+        return dp[currIdx] = ans;
+    }
+
+    public TrieNode buildTrieNode(String[] dictionary) {
+    TrieNode root = new TrieNode();
+    for(String word : dictionary) {
+        TrieNode node = root;
+        for(char ch : word.toCharArray()) {
+            if(node.children[ch-'a'] == null) {
+                node.children[ch-'a'] = new TrieNode();
+            }
+            node = node.children[ch-'a'];
+        }
+        node.isWord = true;
+    
+    }
+    return root;
+}
+
 }
