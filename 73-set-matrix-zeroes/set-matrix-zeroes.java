f6a05374894fe28d5e2e1665745a2912fd1ecb53
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-09-23 20:36:48 +0530

    Time: 1 ms (78.39%) | Memory: 45.7 MB (18.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,29 @@
 class Solution {
     public void setZeroes(int[][] matrix) 
     {
-        int[] row = new int[matrix.length]; 
-        int[] col = new int[matrix[0].length]; 
+        int n = matrix.length; 
+        int m = matrix[0].length; 
+        int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
 
-        for(int i=0; i<matrix.length; i++)
-        {
-            for(int j=0; j<matrix[0].length; j++)
-            {
-                if(matrix[i][j] == 0)
-                {
+        // Step 1: Traverse the matrix and mark rows and columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // Mark the i-th row and j-th column
                     row[i] = 1;
                     col[j] = 1;
                 }
-            }   
+            }
         }
 
-        for(int i=0; i<matrix.length; i++)
-        {
-            for(int j=0; j<matrix[0].length; j++)
-            {
-                if(row[i] == 1 || col[j] == 1)
-                {
+        // Step 2: Set matrix cells to 0 if their row or column is marked
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
                     matrix[i][j] = 0;
                 }
-            }   
-        }
+            }
+        }   
     }
 }
